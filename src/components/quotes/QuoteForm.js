435fef7d7243baa3708fee950f67ad012b2acb73
K-Mo2 <karimelsisi27@gmail.com>
2021-10-24 23:37:12 +0200

    Improved user experience through protection against random unintended clicks

--- src/components/quotes/QuoteForm.js
+++ src/components/quotes/QuoteForm.js
@@ -1,12 +1,14 @@
-import { useRef } from 'react';
+import { useRef, Fragment, useState } from "react";
+import { Prompt } from "react-router-dom";
 
-import Card from '../UI/Card';
-import LoadingSpinner from '../UI/LoadingSpinner';
-import classes from './QuoteForm.module.css';
+import Card from "../UI/Card";
+import LoadingSpinner from "../UI/LoadingSpinner";
+import classes from "./QuoteForm.module.css";
 
 const QuoteForm = (props) => {
   const authorInputRef = useRef();
   const textInputRef = useRef();
+  const [inputIsValid, setInputIsValid] = useState(true);
 
   function submitFormHandler(event) {
     event.preventDefault();
@@ -19,28 +21,49 @@ const QuoteForm = (props) => {
     props.onAddQuote({ author: enteredAuthor, text: enteredText });
   }
 
+  const inputBlurHandler = (event) => {
+    if (event.target.value.trim() === "") {
+      setInputIsValid(false);
+    }
+  };
   return (
-    <Card>
-      <form className={classes.form} onSubmit={submitFormHandler}>
-        {props.isLoading && (
-          <div className={classes.loading}>
-            <LoadingSpinner />
-          </div>
-        )}
+    <Fragment>
+      <Prompt
+        when={!inputIsValid}
+        message="Are you sure you want to leave this page"
+      />
+      <Card>
+        <form className={classes.form} onSubmit={submitFormHandler}>
+          {props.isLoading && (
+            <div className={classes.loading}>
+              <LoadingSpinner />
+            </div>
+          )}
 
-        <div className={classes.control}>
-          <label htmlFor='author'>Author</label>
-          <input type='text' id='author' ref={authorInputRef} />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor='text'>Text</label>
-          <textarea id='text' rows='5' ref={textInputRef}></textarea>
-        </div>
-        <div className={classes.actions}>
-          <button className='btn'>Add Quote</button>
-        </div>
-      </form>
-    </Card>
+          <div className={classes.control}>
+            <label htmlFor="author">Author</label>
+            <input
+              type="text"
+              id="author"
+              ref={authorInputRef}
+              onBlur={inputBlurHandler}
+            />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor="text">Text</label>
+            <textarea
+              id="text"
+              rows="5"
+              ref={textInputRef}
+              onBlur={inputBlurHandler}
+            ></textarea>
+          </div>
+          <div className={classes.actions}>
+            <button className="btn">Add Quote</button>
+          </div>
+        </form>
+      </Card>
+    </Fragment>
   );
 };
 
